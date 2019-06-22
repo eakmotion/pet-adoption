b964721d493d2cbd3405c020f3e2b55fa8df63ba
eakmotion <eakpun@gmail.com>
2019-06-23 01:59:48 +0700

    Add Reach Router to handle routing pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Link } from '@reach/router';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
   return (
     <div>
-      <h1>Pet Adoption</h1>
-      <SearchParams />
+      <header>
+        <Link to='/'>
+          <h1>Pet Adoption</h1>
+        </Link>
+      </header>
+      <Router>
+        <SearchParams path='/' />
+        <Details path='/details/:id' />
+      </Router>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
